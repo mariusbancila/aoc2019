1d8615634e510f2013cd2a3a51af68b604e4294a
Marius Bancila <marius.bancila@gmail.com>
2019-12-12 00:30:39 +0200

    more opcodes

--- aoc2019/intcomp/intcomp.cpp
+++ aoc2019/intcomp/intcomp.cpp
@@ -12,6 +12,21 @@
 #include "fmt/core.h"
 #include "fmt/format.h"
 
+constexpr int OP_ADD = 1;
+constexpr int OP_MUL = 2;
+constexpr int OP_IN = 3;
+constexpr int OP_OUT = 4;
+constexpr int OP_JMPNZ = 5;
+constexpr int OP_JMPZ = 6;
+constexpr int OP_LS = 7;
+constexpr int OP_EQ = 8;
+constexpr int OP_BASEOFF = 9;
+constexpr int OP_HALT = 99;
+
+constexpr int MOD_POSITION = 0;
+constexpr int MOD_IMMEDIATE = 1;
+constexpr int MOD_RELBASE = 2;
+
 using memory_unit = long long;
 using memory_t = std::vector<memory_unit>;
 
@@ -41,6 +56,21 @@ memory_t read_program(std::string_view text)
    return memory;
 }
 
+std::string format_parameter(memory_unit const value, int const mode)
+{
+   switch (mode)
+   {
+   case MOD_POSITION:
+      return fmt::format("[{0}]", value);
+   case MOD_IMMEDIATE:
+      return fmt::format("{0}", value);
+   case MOD_RELBASE:
+      return fmt::format(value > 0 ? "[base+{0}]":"[base{0}]", value);
+   default:
+      throw std::runtime_error("invalid parameter mode");
+   }
+}
+
 void print_program(memory_t const& memory, std::string path)
 {
    std::map<int, std::string> opcodes{ {1, "add"}, {2,"mul"}, {3, "in"}, {4, "out"}, {5, "jnz"}, {6, "jz"}, {7, "le"}, {8, "eq"}, {9, "bso"}, {99, "hlt"} };
@@ -55,42 +85,69 @@ void print_program(memory_t const& memory, std::string path)
       int mod2 = inst % 10; inst /= 10;
       int mod3 = inst % 10; inst /= 10;
 
-      fmt::print("{0:5}", opcodes[opcode]);
+      fmt::print("{0:04x} {1:5}", ip, opcodes[opcode]);
       switch (opcode) 
       {
-      case 1:
-         fmt::print("{0},{1},{2}\n", memory[ip+1], memory[ip+2], memory[ip+3]);
+      case 0:
+         ip++;
+         break;
+      case OP_ADD:
+         fmt::print("{0}, {1}, {2}", 
+            format_parameter(memory[ip+1], mod1), 
+            format_parameter(memory[ip+2], mod2),
+            format_parameter(memory[ip+3], mod3));
          ip += 4;
          break;
-      case 2:
-         fmt::print("{0},{1},{2}\n", memory[ip + 1], memory[ip + 2], memory[ip + 3]);
+      case OP_MUL:
+         fmt::print("{0}, {1}, {2}",
+            format_parameter(memory[ip + 1], mod1),
+            format_parameter(memory[ip + 2], mod2),
+            format_parameter(memory[ip + 3], mod3));
          ip += 4;
          break;
-      case 3:
+      case OP_IN:
+         fmt::print(format_parameter(memory[ip + 1], mod1));
          ip += 2;
          break;
-      case 4:
+      case OP_OUT:
+         fmt::print(format_parameter(memory[ip + 1], mod1));
          ip += 2;
          break;
-      case 5:
+      case OP_JMPNZ:
+         fmt::print("{0}, {1}",
+            format_parameter(memory[ip + 1], mod1),
+            format_parameter(memory[ip + 2], mod2));
          ip += 3;
          break;
-      case 6:
+      case OP_JMPZ:
+         fmt::print("{0}, {1}",
+            format_parameter(memory[ip + 1], mod1),
+            format_parameter(memory[ip + 2], mod2));
          ip += 3;
          break;
-      case 7:
+      case OP_LS:
+         fmt::print("{0}, {1}, {2}",
+            format_parameter(memory[ip + 1], mod1),
+            format_parameter(memory[ip + 2], mod2),
+            format_parameter(memory[ip + 3], mod3));
          ip += 4;
          break;
-      case 8:
+      case OP_EQ:
+         fmt::print("{0}, {1}, {2}",
+            format_parameter(memory[ip + 1], mod1),
+            format_parameter(memory[ip + 2], mod2),
+            format_parameter(memory[ip + 3], mod3));
          ip += 4;
          break;
-      case 9:
+      case OP_BASEOFF:
+         fmt::print("base{0}{1}", memory[ip + 1] > 0 ? "+" : "", memory[ip + 1]);
          ip += 2;
          break;
       case 99:
          ip += 2;
          break;
       }
+      fmt::print("\n");
    }
 }
 
@@ -109,7 +166,7 @@ int main()
       std::istreambuf_iterator<char>(input),
       std::istreambuf_iterator<char>());
 
-   auto memory = read_program("1102, 34915192, 34915192, 7, 4, 7, 99, 0");
+   auto memory = read_program("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
 
    print_program(memory, "program.asm");
 }
