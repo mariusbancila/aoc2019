c990a755f8d1e03a9a9671af4a9e2a8ebb259ec3
Marius Bancila <marius.bancila@gmail.com>
2019-12-15 09:27:49 +0200

    puzzle 14

--- aoc2019/aoc2019_14/aoc2019_14.cpp
+++ aoc2019/aoc2019_14/aoc2019_14.cpp
@@ -9,11 +9,13 @@
 #include <regex>
 #include <algorithm>
 #include <numeric>
+#include <deque>
+#include <set>
 #include <assert.h>
 
 struct chemical_t
 {
-   int         quantity;
+   long long   quantity;
    std::string name;
 };
 
@@ -27,22 +29,13 @@ bool operator<(chemical_t const& t1, chemical_t const& t2)
    return t1.name == t2.name ? t1.quantity < t2.quantity : t1.name < t2.name;
 }
 
-chemical_t operator*(chemical_t const& t1, int const n)
+chemical_t operator*(chemical_t const& t1, long long const n)
 {
    return chemical_t{ t1.quantity * n, t1.name };
 }
 
 using chemicals_collection = std::vector<chemical_t>;
 
-/*
-struct reaction_t
-{
-   chemicals_collection inputs;
-   chemical_t           output;
-};
-
-using reactions_collection = std::vector<reaction_t>;
-*/
 using reactions_collection = std::map<chemical_t, chemicals_collection>;
 
 reactions_collection read_reaction(std::string path)
@@ -76,7 +69,7 @@ reactions_collection read_reaction(std::string path)
                   std::string q = (*iter)[1];
                   std::string n = (*iter)[2];
 
-                  chemicals.push_back(chemical_t{ std::stoi(q), n });
+                  chemicals.push_back(chemical_t{ std::stoll(q), n });
                   ++iter;
                }
             }
@@ -91,7 +84,7 @@ reactions_collection read_reaction(std::string path)
                      std::string q = result[1].str();
                      std::string n = result[2].str();
 
-                     chemical_t chem{std::stoi(q), n};
+                     chemical_t chem{std::stoll(q), n};
 
                      reactions.emplace(chem, chemicals);
                   }
@@ -104,191 +97,27 @@ reactions_collection read_reaction(std::string path)
    return reactions;
 }
 
-int compute_ore(reactions_collection const& collection)
-{
-   auto it = collection.find(chemical_t{1, "FUEL"});
-   if (it == collection.end()) throw std::runtime_error("Fuel reaction not found");
-
-   auto l_is_first_product = [collection](chemical_t const& c)
-   {
-      return std::find_if(
-         collection.begin(), collection.end(),
-         [c](std::pair<chemical_t, chemicals_collection> const &p ) 
-         {
-            return 
-               p.first.name == c.name &&
-               p.second.size() == 1 && p.second.front().name == "ORE";
-         }) != collection.end();
-   };
-
-   auto l_get_reaction = [collection](chemical_t const& c)
-   {
-      auto it = std::find_if(
-         collection.begin(), collection.end(),
-         [c](std::pair<chemical_t, chemicals_collection> const& p)
-         {
-            return p.first.name == c.name;
-         });
-
-      if (it == collection.end()) throw std::runtime_error("chemical not found");
-      return *it;
-   };
-
-   auto l_minimum_factor = [](int const a, int const b)
-   {
-      return a == b ? 1 : static_cast<int>(std::ceil(static_cast<double>(a) / b));
-   };
-
-   auto l_reduce = [](chemicals_collection const& chemicals)
-   {
-      std::map<std::string, int> quantities;
-      for (auto const& c : chemicals)
-      {
-         auto it = quantities.find(c.name);
-         if (it == quantities.end())
-            quantities.insert(std::make_pair(c.name, c.quantity));
-         else
-            it->second += c.quantity;
-      }
-
-      chemicals_collection newchemicals;
-      for (auto const& [n, q] : quantities)
-      {
-         newchemicals.push_back(chemical_t{ q, n });
-      }
-
-      return newchemicals;
-   };
-
-   chemicals_collection chemicals(it->second);
-   do
-   {
-      chemicals_collection newchemicals;
-      for(auto const & c : chemicals)
-      {
-         if (l_is_first_product(c))
-         {
-            newchemicals.push_back(c);
-         }
-         else
-         {
-            auto const & [prod, reactants] = l_get_reaction(c);
-            auto factor = l_minimum_factor(c.quantity, prod.quantity);
-
-            for (auto const& r : reactants)
-            {
-               newchemicals.push_back(r * factor);
-            }
-         }
-      }
-
-      auto result = l_reduce(newchemicals);
-      chemicals.swap(result);
-   } while (
-      !std::all_of(
-         chemicals.begin(), chemicals.end(), 
-         [l_is_first_product](chemical_t const& c) 
-         {
-            return l_is_first_product(c); 
-         }));
-
-   std::map<std::string, int> quantities;
-   for (auto const& c : chemicals)
-   {
-      auto it = quantities.find(c.name);
-      if (it == quantities.end())
-         quantities.insert(std::make_pair(c.name, c.quantity));
-      else
-         it->second += c.quantity;
-   }
-
-   auto total = 0;
-   for (auto const& [name, quantity] : quantities)
-   {
-      auto c = l_get_reaction(chemical_t{ 1, name });
-      assert(c.second.front().name == "ORE");
-
-      auto factor = l_minimum_factor(quantity, c.first.quantity);
-      total += factor * c.second.front().quantity;
-   }
-
-   return total;
-}
-
-int main()
-{
-   //{
-   //   auto reactions = read_reaction(R"(..\data\aoc2019_14_test1.txt)");
-   //   auto ore = compute_ore(reactions);
-   //   assert(ore == 31);
-   //}
-
-   //{
-   //   auto reactions = read_reaction(R"(..\data\aoc2019_14_test2.txt)");
-   //   auto ore = compute_ore(reactions);
-   //   assert(ore == 165);
-   //}
-
-   //{
-   //   auto reactions = read_reaction(R"(..\data\aoc2019_14_test3.txt)");
-   //   auto ore = compute_ore(reactions);
-   //   assert(ore == 13312);
-   //}
-
-   //{
-   //   auto reactions = read_reaction(R"(..\data\aoc2019_14_test4.txt)");
-   //   auto ore = compute_ore(reactions);
-   //   assert(ore == 180697);
-   //}
-
-   //{
-   //   auto reactions = read_reaction(R"(..\data\aoc2019_14_test6.txt)");
-   //   auto ore = compute_ore(reactions);
-   //   assert(ore == 180697);
-   //}
-
-   //{
-   //   auto reactions = read_reaction(R"(..\data\aoc2019_14_test5.txt)");
-   //   auto ore = compute_ore(reactions);
-   //   assert(ore == 2210736);
-   //}
-
-   {
-      auto reactions = read_reaction(R"(..\data\aoc2019_14_input1.txt)");
-      auto ore = compute_ore(reactions);
-      std::cout << ore << '\n';
-   }
-
-   {
-      auto reactions = read_reaction(R"(..\data\aoc2019_14_test7.txt)");
-      auto ore = compute_ore(reactions);
-      assert(ore == 2210736);
-   }
-}
-
-/*
-
-std::pair<int, chemicals_collection> find_reaction(reactions_collection const & reactions, std::string const & name)
+std::pair<long long, chemicals_collection> find_reaction(reactions_collection const& reactions, std::string const& name)
 {
    auto it = std::find_if(reactions.begin(), reactions.end(),
-                          [name](std::pair<chemical_t, chemicals_collection> const & p)
-                          {
-      return p.first.name == name;
-   });
-   
-   if(it == reactions.end()) throw std::runtime_error("invalid chemical");
-   return {it->first.quantity, it->second};
+      [name](std::pair<chemical_t, chemicals_collection> const& p)
+      {
+         return p.first.name == name;
+      });
+
+   if (it == reactions.end()) throw std::runtime_error("invalid chemical");
+   return { it->first.quantity, it->second };
 }
 
-void sort(reactions_collection const & reactions, std::deque<std::string>& order, std::set<std::string>& visited, std::string const & name)
+void sort(reactions_collection const& reactions, std::deque<std::string>& order, std::set<std::string>& visited, std::string const& name)
 {
-   if(name == "ORE") return;
-   
+   if (name == "ORE") return;
+
    visited.insert(name);
-   auto const &[q, r] = find_reaction(reactions, name);
-   for(auto const & c : r)
+   auto const& [q, r] = find_reaction(reactions, name);
+   for (auto const& c : r)
    {
-      if(!visited.count(c.name))
+      if (!visited.count(c.name))
       {
          sort(reactions, order, visited, c.name);
       }
@@ -296,40 +125,131 @@ void sort(reactions_collection const & reactions, std::deque<std::string>& order
    order.push_front(name);
 }
 
-std::deque<std::string> get_order(reactions_collection const & reactions)
+std::deque<std::string> get_order(reactions_collection const& reactions, std::string const & name)
 {
    std::deque<std::string> order;
    std::set<std::string> visited;
-   
-   sort(reactions, order, visited, "FUEL");
-   
+
+   sort(reactions, order, visited, name);
+
    return order;
 }
 
-int get_quantity_needed(int const a, int const b)
+long long get_quantity_needed(long long const a, long long const b)
 {
-   return a == b ? 1 : static_cast<int>(std::ceil(static_cast<double>(a) / b));
+   return a == b ? 1 : static_cast<long long>(std::ceil(static_cast<double>(a) / b));
 };
 
-int compute_ore(reactions_collection const& collection)
+long long compute_ore(reactions_collection const& collection, std::string const & name, long long const quantity)
 {
-   auto it = collection.find(chemical_t{1, "FUEL"});
+   auto it = collection.find(chemical_t{ 1, name });
    if (it == collection.end()) throw std::runtime_error("Fuel reaction not found");
-   
-   std::map<std::string, int> amountNeeded{{"FUEL", 1}};
-   auto order = get_order(collection);
-   for(auto const & name : order)
+
+   std::map<std::string, long long> amountNeeded{ {name, quantity} };
+   auto order = get_order(collection, name);
+   for (auto const& name : order)
    {
-      auto const & [produced, reactions] = find_reaction(collection, name);
-      auto needed = amountNeeded[name];
-      auto factor = get_quantity_needed(needed, produced);
-      
-      for(auto const c : reactions)
+      auto const& [produced, reactions] = find_reaction(collection, name);
+      auto reactions_needed = amountNeeded[name];
+      auto factor = get_quantity_needed(reactions_needed, produced);
+
+      for (auto const c : reactions)
       {
          amountNeeded[c.name] += c.quantity * factor;
       }
    }
-   
+
    return amountNeeded["ORE"];
 }
-*/
+
+long long compute_maximum_fuel(
+   reactions_collection const& reactions, 
+   std::string const & name,
+   long long const ore_total,
+   long long const ore_per_unit)
+{
+   auto fuel = 1ll;
+   auto low = ore_total / ore_per_unit;
+   auto high = low * 2;
+   while (low + 1 < high)
+   {
+      fuel = static_cast<long long>((low + high) / 2);
+      auto cost = compute_ore(reactions, name, fuel);
+      if (cost > ore_total) 
+         high = fuel;
+      else if (cost < ore_total) 
+         low = fuel;
+      else break;
+   }
+
+   if (compute_ore(reactions, name, fuel) > ore_total)
+      fuel--;
+
+   return fuel;
+}
+
+int main()
+{
+   // tests
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test1.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 31);
+   }
+
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test2.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 165);
+   }
+
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test3.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 13312);
+
+      auto fuel = compute_maximum_fuel(reactions, "FUEL", 1000000000000ll, ore);
+      assert(fuel == 82892753);
+   }
+
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test4.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 180697);
+
+      auto fuel = compute_maximum_fuel(reactions, "FUEL", 1000000000000ll, ore);
+      assert(fuel == 5586022);
+   }
+
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test4_1.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 180697);
+   }
+
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test5.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 2210736);
+
+      auto fuel = compute_maximum_fuel(reactions, "FUEL", 1000000000000ll, ore);
+      assert(fuel == 460664);
+   }
+
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_test5_1.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      assert(ore == 2210736);
+   }
+
+   // puzzle
+   {
+      auto reactions = read_reaction(R"(..\data\aoc2019_14_input1.txt)");
+      auto ore = compute_ore(reactions, "FUEL", 1);
+      std::cout << ore << '\n';
+
+      auto fuel = compute_maximum_fuel(reactions, "FUEL", 1000000000000ll, ore);
+      std::cout << fuel << '\n';
+   }
+}
+
